15f9b7b88fe8033b5d9dd4fe3f53f5d00496e1e7
Leighzer <bwc_nothgiel@live.com>
2016-04-06 00:16:19 -0500

    Added files via upload
    Not entirely sure on how to commit as I figured I'd just commit the entire project, but that isn't an option since it is over 25MB. With that I am committing the BAREBONES script to a pickup and the scene that I used to test it.

---
+++ Pickup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            Debug.Log("Player collided with Collectable: " + this.name + "!");
+        }
+       
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+
+
+}
