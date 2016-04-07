aa75a3709e2fb689d3705d977d792b2dab00cf7f
Jordan Hartman <xoshadowhawkox@gmail.com>
2016-04-07 15:22:58 -0500

    Fixed Camera, need to adjust movement for camera change

--- GameDevelopmentClass/Assets/Scripts/Movement.cs
+++ GameDevelopmentClass/Assets/Scripts/Movement.cs
@@ -2,53 +2,103 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-
-    CameraView cameraView;
-
-    public float playerAcceleration = 500f;
-    public float maxSpeed = 20f;
+    
+    
+    public float movementSpeed = 20f;
     public float jumpSpeed = 200f;
-    public float maxSlope = 60f;
-    float deacceleration;
-    float deaccelerationX = 10f;
-    float deaccelerationZ = 10f;
-
-    Vector2 horizontalMovement;
+    public float GUIsize = 100f;
+    float yRotation;
+    float xRotation;
+    float currentXRotation;
+    float currentYRotation;
+    public float GUISize = 50f;
+    float yRotationView;
+    float xRotationView;
+    public float rotationSmoothness = .1f;
 
     public GameObject playerCamera;
     public GameObject player;
+    public CharacterController controller;
+    
 
     bool onGround = false;
 
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+    }
 
-        horizontalMovement = new Vector2(player.GetComponent<Rigidbody>().velocity.x, player.GetComponent<Rigidbody>().velocity.z);
+    // Update is called once per frame
+    void Update () {
 
-        if(horizontalMovement.magnitude > maxSpeed)
+        //rotates player
+        rotatePlayer();
+
+        //move to postion
+        moveToPosition();
+
+    }
+
+    //moves character, all movements are opposite in game as the mape was creates backwards(ie: only way to compensate)
+    void moveToPosition()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            horizontalMovement = horizontalMovement.normalized;
-            horizontalMovement *= maxSpeed;
+            controller.transform.position += Vector3.left * movementSpeed * Time.deltaTime;
         }
 
-        //work in progress, Slows down player object at a faster rate
-        /*player.GetComponent<Rigidbody>().velocity.x = horizontalMovement.x;
-        player.GetComponent<Rigidbody>().velocity.z = horizontalMovement.y;
-
-        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            player.GetComponent<Rigidbody>().velocity.x = Mathf.SmoothDamp(player.GetComponent<Rigidbody>().velocity.x, 0f, ref deaccelerationX, deacceleration);
-            player.GetComponent<Rigidbody>().velocity.z = Mathf.SmoothDamp(player.GetComponent<Rigidbody>().velocity.z, 0f, ref deaccelerationZ, deacceleration);
-        }*/
-
-        //work in progress, will be fixed once i can see my older code. down side of being in mexico, no desktop :(
-        //cameraView.transform.rotation = Quaternion.Euler(0f, cameraView.currentYRotation, 0f);
-        player.GetComponent<Rigidbody>().AddRelativeForce(Input.GetAxis("Horizontal") * playerAcceleration* Time.deltaTime, 0, Input.GetAxis("Vertical") * playerAcceleration * Time.deltaTime);
-
-        //the jumping action, work in progress as this is a first for me and i have nothing but email in mexico
-        if(Input.GetButtonDown("Jump") && onGround)
-        {
-            player.GetComponent<Rigidbody>().AddForce(0f, jumpSpeed, 0f);
+            controller.transform.position += Vector3.right * movementSpeed * Time.deltaTime;
         }
-	}
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            controller.transform.position += Vector3.back * movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            controller.transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
+        }
+    }
+
+    //rotates player
+    void rotatePlayer()
+    {
+        var recdown = new Rect(0f, 0f, Screen.width, GUISize);
+        var recup = new Rect(0, Screen.height - GUISize, Screen.width, GUISize);
+        var recleft = new Rect(0, 0, GUISize, Screen.height);
+        var recright = new Rect(Screen.width - GUISize, 0, GUISize, Screen.height);
+
+        if (recup.Contains(Input.mousePosition))
+        {
+            xRotation = Mathf.SmoothDamp(currentXRotation, currentXRotation++, ref xRotationView, rotationSmoothness);
+            xRotation = Mathf.Clamp(xRotation, -60f, 80f);
+            controller.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        }
+
+        if (recdown.Contains(Input.mousePosition))
+        {
+            xRotation = Mathf.SmoothDamp(currentXRotation, currentXRotation--, ref xRotationView, rotationSmoothness);
+            xRotation = Mathf.Clamp(xRotation, -60f, 80f);
+            controller.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        }
+
+        if (recright.Contains(Input.mousePosition))
+        {
+            //yRotation = Input.GetAxis("Mouse Y") * rotationSmoothness;
+            yRotation = Mathf.SmoothDamp(currentYRotation, currentYRotation++, ref yRotationView, rotationSmoothness);
+
+            controller.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        }
+
+        if (recleft.Contains(Input.mousePosition))
+        {
+            //yRotation = Input.GetAxis("Mouse Y") * rotationSmoothness;
+            yRotation = Mathf.SmoothDamp(currentYRotation, currentYRotation--, ref yRotationView, rotationSmoothness);
+
+            controller.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        }
+    }
+
 }
