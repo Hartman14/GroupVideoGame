8b75680efb9adb7e2843cd57ea91f610a1f55631
dan <dan@Dans-MacBook-Pro.local>
2016-04-07 21:30:53 -0500

    import of mainmenu
    Imported main menu in to recent master.  Work on load menu,  Load Menu
    will now let you select different scenes for now. New game will open
    grants scene.

---
+++ GameDevelopmentClass/Assets/DropDownList.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+public class DropDownList : MonoBehaviour {
+
+	public List<string> testList = new List<string> ();
+	public Dropdown DropDownListObject;
+
+
+	// Use this for initialization
+
+	void Start () {
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void testUpdateList(){
+
+		testList.Add ("Documentation sucks");
+		DropDownListObject.AddOptions (testList);
+	}
+
+}
