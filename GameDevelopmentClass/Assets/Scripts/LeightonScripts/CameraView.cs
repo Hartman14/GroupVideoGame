b0bb01a3ce4673a5a7a65090a90df724d43d0153
Grant Moe <grantmoe2@gmail.com>
2016-04-15 13:13:49 -0500

    LeightonApril15UpdateScripts
    -Implemented food pickup, armor, gold, and poison pickup. This pickups
    affect the player inventory appopriately to their name, and then delete
    themselves. Also made some simple materials(colors) to provide basic
    color to differentiate the pickups.

---
+++ GameDevelopmentClass/Assets/Scripts/LeightonScripts/CameraView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraView: MonoBehaviour {
+	// public float lookSensitivity = 5f;
+	float yRotation;
+	float xRotation;
+	public float currentXRotation;
+	public float currentYRotation;
+	float yRotationView;
+	float xRotationView;
+	public float rotationSmoothness = 0.1f;
+    public GameObject cameraView;
+
+	// Update is called once per frame
+	void Update () {
+	
+		yRotation = Input.GetAxis ("Mouse Y") * rotationSmoothness;
+		xRotation = Input.GetAxis ("Mouse X") * rotationSmoothness;
+
+		xRotation = Mathf.Clamp (xRotation, -60f, 80f);
+
+		currentXRotation = Mathf.SmoothDamp (currentXRotation, xRotation, ref xRotationView, rotationSmoothness);
+		currentYRotation = Mathf.SmoothDamp (currentYRotation, yRotation, ref yRotationView, rotationSmoothness);
+
+		cameraView.transform.rotation = Quaternion.Euler (xRotation, yRotation, 0f);
+
+	}
+}
