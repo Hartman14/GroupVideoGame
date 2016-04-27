663f292023537ca3d25461104b2d931c9f572836
damonhage <damon.hage@live.com>
2016-04-27 12:53:37 -0500

    huftuvj

---
+++ GameDevelopmentClass/Assets/Scripts/DestructableObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructableObject : MonoBehaviour
+{
+
+    int hitcount;
+    int hitsToKill;
+    // Use this for initialization
+    void Start()
+    {
+        hitcount = 0;
+        hitsToKill = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Sword")
+        {
+            hitcount++;
+        }
+        if (hitcount == hitsToKill)
+        {
+            death();
+        }
+    }
+
+    void death()
+    {
+        Destroy(gameObject);
+    }
+}
